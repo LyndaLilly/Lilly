ac755295140a45a25c5f6682abe6af25d3e764f3
Lynda Ndidiamaka <116148645+LyndaLilly@users.noreply.github.com>
2023-03-26 18:31:18 +0100

    Add files via upload

---
+++ sidebar/sidebar.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(e){
+    
+    const menu_toggle = document.querySelector('.menu-toggle');
+    const sidebar = document.querySelector('.sidebar');
+  
+
+    menu_toggle.addEventListener('click',()=>{
+    menu_toggle.classList.toggle('is-active');
+    sidebar.classList.toggle('is-active');
+
+    })
+});
